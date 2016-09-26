f37365da298e94ab159861b86d94ab22a5fdddbb
Jeremy <j.leclercq@webdigit.be>
2016-09-26 09:20:09 +0200

    Affichage wd_attr_displ_position suivant la valeur de wd_attr_displ_type + idem pour wd_attr_displ_outstock

---
+++ views/js/wdattrdispl_admin.js
@@ -0,0 +1,31 @@
+$(function(){
+	
+	$('#wd_attr_displ_type').bind('change',function(e){
+		var value = $(this).val();
+		switch (value){
+		case 'static':
+			$('#wd_attr_displ_position').closest('.form-group').css('visibility','hidden');
+			//$('#wd_attr_displ_position').closest('.form-group').hide();
+			break;
+		case 'hover' :
+			$('#wd_attr_displ_position').closest('.form-group').css('visibility','visible');
+			//$('#wd_attr_displ_position').closest('.form-group').show();
+			break;
+			default:break;
+		}
+	});
+	
+	$('#wd_attr_displ_format').bind('change', function(e){
+		var value = $(this).val();
+		switch (value){
+		case 'attribut' :
+			$('#wd_attr_displ_outstock').closest('.form-group').css('visibility', 'hidden');
+			break;
+		case 'combinaison' :
+			$('#wd_attr_displ_outstock').closest('.form-group').css('visibility', 'visible');
+			break;
+			default:break;
+		}
+	});
+
+})
